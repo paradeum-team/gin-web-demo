c76bd911c9ff1a59c3f366142770c16a89b5f7c6
xcdong <dongxucheng3@126.com>
2020-02-24 16:29:19 +0800

    resty post

--- base/resty_client.go
+++ base/resty_client.go
@@ -13,10 +13,27 @@ func RestyGet(url string, token string) ([]byte, error) {
 		fmt.Printf("the error is %v ", err)
 		return nil, err
 	}
-
+	defer resp.RawResponse.Body.Close()
 	body := resp.Body()
 	//body, err := ioutil.ReadAll(response.Body)
 
 	fmt.Printf("body=%s \n", string(body))
+
+	return body, nil
+}
+
+
+func RestyPost(url string,data interface{} ,token string) ([]byte, error) {
+	resp, err := resty.SetTimeout(time.Duration(60) * time.Second).R().SetBody(data).Get(url)
+	if err != nil {
+		fmt.Printf("the error is %v ", err)
+		return nil, err
+	}
+	defer resp.RawResponse.Body.Close()
+	body := resp.Body()
+	//body, err := ioutil.ReadAll(response.Body)
+
+	fmt.Printf("body=%s \n", string(body))
+
 	return body, nil
 }
