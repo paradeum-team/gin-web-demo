64859cb450f63e0eebe260d4fa0543958e0ef1fc
xcdong <dongxucheng3@126.com>
2020-03-18 15:57:44 +0800

    update the http get

--- base/http_client.go
+++ base/http_client.go
@@ -11,37 +11,40 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"mime/multipart"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
-func HttpGet(url string,token string ) ([] byte, error) {
-	method:=dict.HttpGet
-	return httpExecute4JSON(method,url,"",token)
+func HttpGet(url string, token string) ([] byte, error) {
+	method := dict.HttpGet
+	return httpExecute4JSON(method, url, "", token)
 }
 
-func HttpDelete(url string,token string ) ([] byte, error) {
-	method:=dict.HttpDelete
-	return httpExecute4JSON(method,url,"",token)
+func HttpDelete(url string, token string) ([] byte, error) {
+	method := dict.HttpDelete
+	return httpExecute4JSON(method, url, "", token)
 }
 
-func HttpPut(url string,jsonBody string,token string ) ([] byte, error) {
-	method:=dict.HttpPut
-	return httpExecute4JSON(method,url,jsonBody,token)
+func HttpPut(url string, jsonBody string, token string) ([] byte, error) {
+	method := dict.HttpPut
+	return httpExecute4JSON(method, url, jsonBody, token)
 }
 
-func HttpPost(url string,jsonBody string,token string ) ([] byte, error) {
-	method:=dict.HttpPost
-	return httpExecute4JSON(method,url,jsonBody,token)
+func HttpPost(url string, jsonBody string, token string) ([] byte, error) {
+	method := dict.HttpPost
+	return httpExecute4JSON(method, url, jsonBody, token)
 }
 
-func HttpForm(url string,params map[string]interface{}, filePath string, token string) ([] byte, error){
-	method:=dict.HttpPost
-	return httpExecute4PostFile(method,url,params,filePath,token)
+func HttpForm(url string, params map[string]string, filePath string, token string) ([] byte, error) {
+	method := dict.HttpPost
+	return httpExecute4PostFile(method, url, params, filePath, token)
 }
+
 /**
  * 获取 http client
  * 忽略证书
@@ -62,9 +65,10 @@ func getHttpClient() *http.Client {
 	if pldconf.AppConfig.Server.ProxyModel { //本地开发模式，需要使用代理
 		tr.Proxy = http.ProxyURL(proxy)
 	}
+	timeout:= 60
 	client := &http.Client{
 		Transport: tr,
-		//Timeout:   time.Second * 5, //超时时间
+		Timeout:   time.Second * time.Duration(timeout), //超时时间
 	}
 	return client
 }
@@ -74,21 +78,22 @@ func getHttpClient() *http.Client {
  */
 func httpExecute4JSON(httpMethod string, url string, jsonParams string, auth string) ([] byte, error) {
 	var requestBody *strings.Reader //默认值 nil
-	if   len(jsonParams) > 0 {
+	if len(jsonParams) > 0 {
 		requestBody = strings.NewReader(jsonParams)
 	}
 
-	return httpExcute(httpMethod,url,requestBody,auth)
+	return httpExe4JSON(httpMethod, url, requestBody, auth)
 }
+
 /**
 * http client get request
 * return httpStatusCode ,body ,err
 */
-func httpExecute4PostFile(httpMethod string,url string,params map[string]interface{}, filePath string, auth string) ([] byte, error) {
+func httpExecute4PostFile(httpMethod string, url string, params map[string]string, filePath string, auth string) ([] byte, error) {
 	var message = ""
 	file, err := os.Open(filePath)
 	if err != nil {
-		message=fmt.Sprintf("[HttpExecute method=POST]  open file is  error. %v \n",err)
+		message = fmt.Sprintf("[HttpExecute method=POST]  open file is  error. %v \n", err)
 		log.Println(message)
 		return nil, errors.New(message)
 	}
@@ -98,73 +103,84 @@ func httpExecute4PostFile(httpMethod string,url string,params map[string]interfa
 	writer := multipart.NewWriter(requestBody)
 	part, err := writer.CreateFormFile("file", getTempPath()+file.Name())
 	if err != nil {
-		message=fmt.Sprintf("[HttpExecute method=POST]  CreateFormFile is  wrong. %v \n",err)
+		message = fmt.Sprintf("[HttpExecute method=POST]  CreateFormFile is  wrong. %v \n", err)
 		return nil, err
 	}
 	_, err = io.Copy(part, file)
 
 	for key, val := range params {
-		switch v := val.(type) {
-		case string:
-			_ = writer.WriteField(key, v)
-		case int:
-			var s int
-			s = v
-			v2 := strconv.Itoa(s)
-			_ = writer.WriteField(key, v2)
-		case bool:
-			var s bool
-			s = v
-			v2 := strconv.FormatBool(s)
-			_ = writer.WriteField(key, v2)
-		default:
-			message=fmt.Sprintf("convert type error.the param type  must be in [string,int ,bool]")
-			log.Println(message)
-			return nil,errors.New(message)
-		}
-
+		_ = writer.WriteField(key, val)
 	}
 	err = writer.Close()
 	if err != nil {
-		message=fmt.Sprintf("[HttpPostExecute]  close the temp writer is  wrong. %v \n",err)
+		message = fmt.Sprintf("[HttpPostExecute]  close the temp writer is  wrong. %v \n", err)
 		return nil, err
 	}
-
-	return httpExcute(httpMethod,url,requestBody,auth)
+	contentType := writer.FormDataContentType()
+	return httpExecute4Form(httpMethod, url, contentType, requestBody, auth)
 
 }
 
+func httpExe4JSON(httpMethod string, url string, requestBody io.Reader, auth string) ([] byte, error) {
+	contentType := "application/json"
 
-func httpExcute(httpMethod string, url string, requestBody io.Reader, auth string) ([] byte, error) {
+	return httpExcute(httpMethod, url, contentType, requestBody, auth)
+}
+
+func httpExecute4Form(httpMethod string, url string, contentType string, requestBody io.Reader, auth string) ([] byte, error) {
+	return httpExcute(httpMethod, url, contentType, requestBody, auth)
+}
+func httpExcute(httpMethod string, url string, contentType string, requestBody io.Reader, auth string) ([] byte, error) {
 	client := getHttpClient()
 	var message = ""
-	req, err := http.NewRequest(httpMethod, url, requestBody)
+	var req *http.Request
+	var err error
+
+	if dict.HttpGet==httpMethod{
+		req, err = http.NewRequest(httpMethod, url, nil)
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
+	}else {
+		req, err = http.NewRequest(httpMethod, url, requestBody)
+		req.Header.Set("Content-Type", contentType)
+
+	}
 	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Content-Type", "application/json")
+
+
 	if auth != "" {
 		req.Header.Set("Authorization", auth)
 	}
 
 	response, err := client.Do(req)
-
 	if err != nil {
-		message=fmt.Sprintf("请求错误：%v", err)
+		message = fmt.Sprintf("请求错误：%v", err)
+		log.Println(message)
+
+		return nil, errors.New(message)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		message = fmt.Sprintf("服务器响应错误：%v", err)
 		log.Println(message)
 		return nil, errors.New(message)
 	}
-	defer response.Body.Close()
+
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		message=fmt.Sprintf("读取body失败：%v", err)
+		message = fmt.Sprintf("读取body失败：%v", err)
 		log.Println(message)
 		return nil, errors.New(message)
 	}
 
-	return   body, nil
+	return body, nil
 }
-func getTempPath()string {
-
-	timeContent:=utils.GetCurrentTimeUnix()
-	return "./data/temp/" +timeContent
+func getTempPath() string {
+	//rand.Seed(time.Now().UnixNano())
+	timeContent := utils.GetCurrentTimeUnix()
+	randnum := strconv.Itoa(rand.Intn(9223372036854775805))
+	filename := timeContent + randnum
+	return "./data/temp/" + filename
 
 }
